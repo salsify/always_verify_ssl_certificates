a94efcd753e66fe9dc011a23eb008440e1a533cf
Daniel Spangenberger <dspangenberger@salsify.com>
2014-08-21 11:09:28 -0400

    use net::http v 2.0 base code

--- lib/always_verify_ssl_certificates.rb
+++ lib/always_verify_ssl_certificates.rb
@@ -5,40 +5,75 @@ require "net/https"
 
 module Net
   class HTTP
-    private
-      def connect
-        D "opening connection to #{conn_address()}..."
-        s = timeout(@open_timeout) { TCPSocket.open(conn_address(), conn_port()) }
-        D "opened"
-        if use_ssl?
-          if self.verify_mode != OpenSSL::SSL::VERIFY_PEER
-            self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-          end
-          s = OpenSSL::SSL::SSLSocket.new(s, @ssl_context)
-          s.sync_close = true
+  private
+    def connect
+
+      if proxy? then
+        conn_address = proxy_address
+        conn_port    = proxy_port
+      else
+        conn_address = address
+        conn_port    = port
+      end
+
+      D "opening connection to #{conn_address}:#{conn_port}..."
+      s = Timeout.timeout(@open_timeout, Net::OpenTimeout) {
+        TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
+      }
+      D "opened"
+      if use_ssl?
+        if self.verify_mode != OpenSSL::SSL::VERIFY_PEER
+          self.verify_mode = OpenSSL::SSL::VERIFY_PEER
         end
-        @socket = BufferedIO.new(s)
-        @socket.read_timeout = @read_timeout
-        @socket.debug_output = @debug_output
-        if use_ssl?
+
+        ssl_parameters = Hash.new
+        iv_list = instance_variables
+        SSL_IVNAMES.each_with_index do |ivname, i|
+          if iv_list.include?(ivname) and
+            value = instance_variable_get(ivname)
+            ssl_parameters[SSL_ATTRIBUTES[i]] = value if value
+          end
+        end
+        @ssl_context = OpenSSL::SSL::SSLContext.new
+        @ssl_context.set_params(ssl_parameters)
+        D "starting SSL for #{conn_address}:#{conn_port}..."
+        s = OpenSSL::SSL::SSLSocket.new(s, @ssl_context)
+        s.sync_close = true
+        D "SSL established"
+      end
+      @socket = BufferedIO.new(s)
+      @socket.read_timeout = @read_timeout
+      @socket.continue_timeout = @continue_timeout
+      @socket.debug_output = @debug_output
+      if use_ssl?
+        begin
           if proxy?
-            @socket.writeline sprintf('CONNECT %s:%s HTTP/%s',
-                                      @address, @port, HTTPVersion)
-            @socket.writeline "Host: #{@address}:#{@port}"
+            buf = "CONNECT #{@address}:#{@port} HTTP/#{HTTPVersion}\r\n"
+            buf << "Host: #{@address}:#{@port}\r\n"
             if proxy_user
               credential = ["#{proxy_user}:#{proxy_pass}"].pack('m')
               credential.delete!("\r\n")
-              @socket.writeline "Proxy-Authorization: Basic #{credential}"
+              buf << "Proxy-Authorization: Basic #{credential}\r\n"
             end
-            @socket.writeline ''
+            buf << "\r\n"
+            @socket.write(buf)
             HTTPResponse.read_new(@socket).value
           end
-          s.connect
+          s.session = @ssl_session if @ssl_session
+          # Server Name Indication (SNI) RFC 3546
+          s.hostname = @address if s.respond_to? :hostname=
+          Timeout.timeout(@open_timeout, Net::OpenTimeout) { s.connect }
           if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
             s.post_connection_check(@address)
           end
+          @ssl_session = s.session
+        rescue => exception
+          D "Conn close because of connect error #{exception}"
+          @socket.close if @socket and not @socket.closed?
+          raise exception
         end
-        on_connect
       end
+      on_connect
+    end
   end
 end
